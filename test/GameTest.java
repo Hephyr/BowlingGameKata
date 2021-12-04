48dd236362687d969ab33ebac7d6e7c3aa9f4aa0
Hefeng Xu <ixuhefeng@gmail.com>
2021-12-04 16:59:56 +0800

    Feat: all ones game test

--- test/GameTest.java
+++ test/GameTest.java
@@ -11,4 +11,13 @@ class GameTest {
         }
         assertEquals(0, game.score());
     }
+
+    @Test
+    public void testAllOnes() {
+        Game game = new Game();
+        for (int i = 0; i < 20; i++) {
+            game.roll(1);
+        }
+        assertEquals(20, game.score());
+    }
 }
